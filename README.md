69ad1fdd02633a4009cbfb3c2d3d0452bf5224b3
xuyoujiang <xuyoujiang@xuyoujiangdeMacBook-Pro.local>
2018-05-11 01:18:23 +0800

    modify read me, add logits, add pretrained model

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This is an implementation of Sequential VLAD (SeqVLAD) in PyTorch.
 Otherwise you will not be able to use the inception series CNN archs.
 
 
+
+
 ## Training
 
 To train a new model, use the `main.py` script.
@@ -72,6 +74,23 @@ python /mnt/lustre/xuyoujiang/action/seqvlad-pytorch/main.py ucf101 Flow ./data/
    --optim ${optim}
 ```
 
+
+
+### TSN Pretrained Model
+
+**For the Flow stream, we utilize the tsn pretrained model to initialize our model.** Thus, we release the pretrained tsn models to easily reprodct our method. The pretrained models are released as follows: (the model is reimplement by us, not official model. But the performance is comparable.)
+
+| Model  | Modality | Split | Link                                                         |
+| ------ | -------- | ----- | ------------------------------------------------------------ |
+| HMDB51 | RGB      | 1     | [hmdb51_bninception_split1_rgb_model_best.pth](https://tjueducn-my.sharepoint.com/:u:/g/personal/yjxu_tju_edu_cn/EXgsTfOClKlBvWygavN1BzgBNyLPd-Ukn7jBNj3t88q7jA?e=eu6PL0) |
+| HMDB51 | RGB      | 2     | [hmdb51_bninception_split2_rgb_model_best.pth](https://tjueducn-my.sharepoint.com/:u:/g/personal/yjxu_tju_edu_cn/ETfn6SCeKqJBt3tDdA7JtK4BLdcSdYcnvXnL9RUJ9vUW9A?e=jvUjNG) |
+| HMDB51 | RGB      | 3     | [hmdb51_bninception_split3_rgb_model_best.pth](https://tjueducn-my.sharepoint.com/:u:/g/personal/yjxu_tju_edu_cn/EWOThQmEPR5HjQStDJsyUbEB-1Zgjz_Jcf-bdR0AMuAhKA?e=rlgIrv) |
+| HMDB51 | Flow     | 1     | [hmdb51_bninception_split1_flow_model_best.pth](https://tjueducn-my.sharepoint.com/:u:/g/personal/yjxu_tju_edu_cn/EXgsTfOClKlBvWygavN1BzgBNyLPd-Ukn7jBNj3t88q7jA?e=E5neqz) |
+| HMDB51 | Flow     | 2     | [hmdb51_bninception_split2_flow_model_best.pth](https://tjueducn-my.sharepoint.com/:u:/g/personal/yjxu_tju_edu_cn/ERAS2JK9BSZCry_l1Ndtv6kB4rBdS-_vna_JNQ_Jast8OQ?e=1OUFel) |
+| HMDB51 | Flow     | 3     | [hmdb51_bninception_split3_flow_model_best.pth](https://tjueducn-my.sharepoint.com/:u:/g/personal/yjxu_tju_edu_cn/EezS5HNGwf1IseTjKXcJ5IIBxjstXfC94AXYbzfmAFR5cw?e=hy3fhY) |
+
+
+
 ## Testing
 
 After training, there will checkpoints saved by pytorch, for example `ucf101_rgb_split1_checkpoint.pth`.
@@ -109,6 +128,51 @@ python test_models.py ucf101 Flow ./data/ucf101_splits/flow/test_split${split}.t
 
 **Note**: We first build our SeqVLAD on the repository of [old-seqvlad-pytorch](https://github.com/youjiangxu/tsn-pytorch/tree/seqvlad), which is folked from [tsn-pytorch](https://github.com/yjxiong/tsn-pytorch). And in order to reproduct our method easily, we release the source code in this repository [SeqVLAD-Pytorch](https://github.com/youjiangxu/seqvlad-pytorch).
 
+
+
+## Quick Fusion
+
+If you're only looking for our final last-layer features that can be combined with your method, we provide those for the following dataset:
+
+```bash
+./logits/hmdb51/
+./logits/ucf101/
+```
+
+For example, you can use following command to merge two modality results (e.g., RGB+Flow) and obtain the final accuracy on HMDB51 split1.
+
+```bash
+python ./merge.py --rgb ./logits/hmdb51/hmdb51_rgb_split1.npz --flow ./logits/hmdb51/hmdb51_flow_split1.npz
+```
+
+
+
+The results is as follows:
+
+###HMDB51
+
+| Split   | RGB   | Flow  | RGB+Flow |
+| ------- | ----- | ----- | -------- |
+| 1       | 55.23 | 65.36 | 72.88    |
+| 2       | 54.31 | 74.77 | 70.39    |
+| 3       | 53.66 | 65.49 | 71.18    |
+| Average | 54.4  | 65.20 | 71.48    |
+
+### UCF101 -TODO
+
+| Split | RGB  | FLow | RGB+FLow |
+| ----- | ---- | ---- | -------- |
+|       |      |      |          |
+|       |      |      |          |
+|       |      |      |          |
+|       |      |      |          |
+
+
+
 ### Useful Links
 - [tsn-pytorch](https://github.com/yjxiong/tsn-pytorch)
 - [tsn-caffe](https://github.com/yjxiong/temporal-segment-networks)
+
+## Updates
+
+- 2018-05-11 upload the pretrained tsn model, add logits.
--- merge.py
+++ merge.py
@@ -1,5 +1,5 @@
 import os
-import numpy as np
+import numpy as np 
 
 import argparse
 
@@ -12,7 +12,59 @@ parser.add_argument('--flow', type=str, default=None)
 parser.add_argument('--idt', type=str, default=None)
 parser.add_argument('--weight' ,type=float, default=[1., 1.5], nargs="+")
 parser.add_argument('--idt_wt', type=float, default=0.25)
+parser.add_argument('--split', type=str, default='1')
 args = parser.parse_args()
+
+
+def load_idt_thing():
+    re_names = []
+    with open('./data/hmdb51_splits/AllVideos_renamed.txt', 'r') as rf:
+        for idx, line in enumerate(rf):
+            re_names.append(line.strip())
+    ori_names = {}
+    with open('./data/hmdb51_splits/AllVideos.txt', 'r') as rf:
+        for idx, line in enumerate(rf):
+
+            temp = line.strip().split('/')
+            ori_names[idx+1] = temp[1][:-4]
+    #print(len(ori_names))
+    #print(len(re_names))
+    #assert len(ori_names)==len(re_names)
+    rename2ori = {}
+    for idx, re_name in enumerate(re_names):
+        #print(re_name, ori_name)
+        line_id = int(re_name.split('/')[1])
+        rename2ori[re_name] = ori_names[line_id]
+            
+    test_names = {}
+    with open('./data/hmdb51_splits/test_split'+args.split+'.txt', 'r') as rf:
+        for idx, line in enumerate(rf):
+            temp = line.strip().split(' ')
+            test_names[idx] = temp[0]
+    #print(test_names)
+    return rename2ori, test_names
+    #with open('./data/hmdb51_splits/'
+def load_idt(idt_path):
+    if idt_path is not None:
+      import scipy.io as sio
+      boxes = sio.loadmat(idt_path)
+      #print(boxes.keys())
+      #idt_score =  boxes['scores_idt']
+      #print(idt_score)
+      
+      idt_map = {}
+      for idx, (name, idt_score) in enumerate(zip(boxes['vn_idt'], boxes['scores_idt'])):
+         #print(idt_score.shape)
+         #print(name[0][0])
+         #print(type(name[0].astype(str))) 
+         idt_map[name[0][0]]=idt_score
+          
+      #print(type(idt_score))
+      #print(boxes['vn_idt'][0:10])
+      return idt_map, boxes['scores_idt']
+      #print(idt.shape)
+      #with h5py.File(args['idt_scores'], 'r') as fin:
+      #  idt = fin['idt'].value
 
 def load_score(file_path):
     score_info = np.load(file_path)
@@ -23,51 +75,28 @@ def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
 
-def load_idt(idt_path):
-    if idt_path is not None:
-      import scipy.io as sio
-      boxes = sio.loadmat(idt_path)
-      #print(boxes.keys())
-      idt =  boxes['scores_idt']
-      #print(boxes['vn_idt'][0:10])
-      #print(idt.shape)
-      #with h5py.File(args['idt_scores'], 'r') as fin:
-      #  idt = fin['idt'].value
-    else:
-      idt = np.zeros(final.shape)
-    return idt
-
 def compute_acc(rgb_score, flow_score, labels):
 
     acc = 0.
-    #acc_onlyIDT = np.mean(idt.argmax(axis=1) == labels)
-
-    #idt_wt = args['idt_wt']
-    #final = (1-idt_wt) * (
-    #  final / np.linalg.norm(final, axis=1, keepdims=True)) + idt_wt * (
-    #    idt / np.linalg.norm(idt, axis=1, keepdims=True))
-    #acc_withIDT = np.mean(final.argmax(axis=1) == labels)
-    #print('Spatial = %0.6f [*%f]\nTemporal = %0.6f [*%f]\nFinal acc = '
-    #      '%0.6f.\nonly IDT = %f\nwith IDT = %f' %
-    #      (acc_spat, (1.0 - args['temporal_ratio']), acc_temp, args['temporal_ratio'],
-    #       acc, acc_onlyIDT, acc_withIDT))
-
     if args.idt is not None:
-        idt_score = load_idt(args.idt)
-    for idx, (rgb, flow, label) in enumerate(zip(rgb_score, flow_score, labels)):
+        idt_map, idt_score = load_idt(args.idt)
+        rename2ori, test_names = load_idt_thing()
 
+    for idx, (rgb, flow, label) in enumerate(zip(rgb_score, flow_score, labels)):
+        
         # rgb = softmax(rgb[0][0][0])
         # flow = softmax(flow[0][0][0])
-        #print(rgb.shape)
-        #print(rgb[0].shape)
-        #print(rgb[0][0].shape)
+
         rgb = rgb[0][0][0]
         flow = flow[0][0][0]
-        #print(idt.shape)
+        #print(test_names[idx])
+        #print(rename2ori[test_names[idx]])
+                #idt = idt_score[real_idt_idx]
 
         final_pred = np.asarray(rgb)*args.weight[0] + np.asarray(flow)*args.weight[1]
         if args.idt is not None:
-            idt = idt_score[idx]
+	    idt = idt_map[rename2ori[test_names[idx]]]
+
             final_pred = (1-args.idt_wt) * (
                         final_pred / np.linalg.norm(final_pred, axis=0, keepdims=True)) + args.idt_wt * (
                         idt / np.linalg.norm(idt, axis=0, keepdims=True))
@@ -82,8 +111,15 @@ def compute_acc(rgb_score, flow_score, labels):
     # cls_hit = np.diag(cf)
     # cls_acc = cls_hit / cls_cnt
     # print(cls_acc)
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
+    
     rgb_score, labels = load_score(args.rgb)
     flow_score, labels = load_score(args.flow)
+    
     compute_acc(rgb_score, flow_score, labels)
+
+
+
+
