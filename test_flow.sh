27874548b9fce25a10d3b404a02f289aabd83f84
youjiangxu <youjiangxu@gmail.com>
2018-11-02 11:16:45 +0800

    add test script

---
+++ test_flow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+split=1
+srun --mpi=pmi2 --gres=gpu:1 -n1 -p SenseMediaA --job-name=test python test_models.py ucf101 Flow ./data/ucf101_splits/flow/test_split${split}.txt \
+    models/flow/ucf101_split1_t25_lr0.01_d0.7_e120210250280_f150_softmax_flow_model_best.pth.tar \
+    --arch BNInception \
+    --save_scores results/flow/seqvlad_split${split}_d0.7_t25_e120210250280_f120_lr0.01_s1 \
+    --num_centers 64 \
+    --timesteps 25 \
+    --redu_dim 512 \
+    --sources ../../data/action/ucf-101/ucf101_flow_img_tvl1_gpu \
+    --activation softmax \
+    --test_segments 1 \
+    --flow_pref flow_
+
+
+##    --with_relu \
---
+++ test_rgb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+num_centers=64
+test_segments=1
+seqvlad_type=seqvlad
+#seqvlad_type=bidirect
+#seqvlad_type=unshare_bidirect
+timesteps=25
+split=1
+pref=${seqvlad_type}_t${timesteps}_"d0.8_e80150210_f120_split${split}"
+srun --mpi=pmi2 --gres=gpu:1 -n1 -p SenseMediaA --job-name=test python test_models.py ucf101 RGB ./data/ucf101_splits/rgb/test_split${split}.txt \
+    models/rgb/ucf101_split1_SGD_t25_k64_lr0.02_d0.8_e80150210_f120_soft_tsn_rgb_model_best.pth.tar \
+    --arch BNInception \
+    --save_scores ./results/rgb/seqvlad_rgb_k${num_centers}_s${test_segments}_${pref} \
+    --num_centers ${num_centers} \
+    --timesteps ${timesteps} \
+    --redu_dim 512 \
+    --sources ../../data/action/ucf-101/UCF-101-frames/ \
+    --activation softmax \
+    --seqvlad_type ${seqvlad_type} \
+    --test_segments ${test_segments}
